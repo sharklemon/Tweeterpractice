ccbff2aee1aee887593fe63c2746597a8c13414f
sharklemon <anastasia.l.mark@gmail.com>
2017-08-18 15:05:56 -0400

    first commit basic files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('keys are loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Nr1pXKajVfdAtkiNX7i7U0E41',
+  consumer_secret: 'lbalK5Z3EpdWrnvesWOz7vrkVROTu6ARTmSikTDfylfdRPR3ch',
+  access_token_key: '898613740664389632-xexxnFyqTeG2BOXWbt8JxVR6kxY792o',
+  access_token_secret: 'R8Ly5vFP3G8rmbzdxmj0yGv2GFFa8dPGVOOVnD6oIOYuc',
+}
