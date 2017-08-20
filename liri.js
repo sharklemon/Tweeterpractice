5b0afda33f0072d16b39f2e8035b26cc18b19071
sharklemon <anastasia.l.mark@gmail.com>
2017-08-20 16:48:58 -0400

    infrastructure w/out actual commands

---
+++ liri.js
@@ -0,0 +1,10 @@
+var keysfile = require("./keys.js");
+
+var keys = keysfile.twitterKeys;
+console.log("consumer key test" + keys.consumer_key);
+
+
+// my-tweets
+// spotify-this-song
+// movie-this
+// do-what-it-says
